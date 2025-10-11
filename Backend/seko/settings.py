902d6fcbb3fc7a67a265e7e4941f8d95e1ffc072
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-10-11 07:06:45 +0530

    Completed

--- Backend/seko/settings.py
+++ Backend/seko/settings.py
@@ -128,7 +128,6 @@ DATABASES = {
         'PORT': env('POSTGRES_PORT', default='5432'),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
