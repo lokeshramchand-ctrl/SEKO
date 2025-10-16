11982bb834539bc286f248386770e8e2815aeb07
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-10-16 13:09:51 +0530

    KIKI - 8 (Starting PRODUCTS)

--- Backend/pro/admin.py
+++ Backend/pro/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+# store/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Category, Product
+
+class ProductInline(admin.TabularInline):  
+    model = Product
+    extra = 1  
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    inlines = [ProductInline]  
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price", )
--- Backend/pro/models.py
+++ Backend/pro/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=250)
+  class Meta:
+        verbose_name_plural = "Categories"  # nicer plural name in admin
+
+  def __str__(self):
+        return self.name
+
+  
+class Product(models.Model):
+  name = models.CharField(max_length=250)
+  price = models.DecimalField(max_digits=6 , decimal_places=2)
+  category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
+  
+  def __str__(self):
+    return self.name
