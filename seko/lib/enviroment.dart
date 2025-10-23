16d69947aff446687ca44df58959279021b4ce47
lokeshramhchand-ctrl <lokeshramchand@gmail.com>
2025-10-23 08:31:12 +0530

    KIKI - 17 (STARTING)

--- seko/lib/enviroment.dart
+++ seko/lib/enviroment.dart
@@ -1,7 +1,7 @@
 
 class Environment {
   //TODO : You have to make sure whether the network is connected to web or not
-  static const String baseUrl = 'http://192.168.1.4:8000';
+  static const String baseUrl = 'http://10.231.4.3:8000';
   static const String serverClientId =
       "448503228009-hc7a1q75434l4u9a4sgvl959ri987aso.apps.googleusercontent.com";
 }
